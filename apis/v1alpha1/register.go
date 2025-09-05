95628c94643dcb7508610c9ef33b637572ffb88e
valkiriaaquatica <fernandomendietaovejero@gmail.com>
2025-09-06 01:24:37 +0200

    run hac/prepare.sh

--- apis/v1alpha1/register.go
+++ apis/v1alpha1/register.go
@@ -13,7 +13,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.upbound.io"
+	Group   = "awx-ilijamt.crossplane.io"
 	Version = "v1alpha1"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/valkiriaaquatica/provider-awx-ilijamt
 
 go 1.21
 
